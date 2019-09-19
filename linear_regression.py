eeac542f0e937cd31b16cdb9510df7fd8ebcc1cf
bill-sixun-tang <tangsixun1997@gmail.com>
2019-09-19 13:26:16 -0500

    trial

---
+++ linear_regression.py
@@ -0,0 +1,67 @@
+### Tools for linear regression ###
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+
+
+def simulate_data():
+    """
+    Simulates data for testing linear_regression models.
+    INPUT
+        nobs (int) the number of observations in the dataset
+    RETURNS
+        data (dict) contains X, y, and beta vectors.
+    """
+    pass
+
+
+def compare_models():
+    """
+    Compares output from different implementations of OLS.
+    INPUT
+        X (ndarray) the independent variables in matrix form
+        y (array) the response variables vector
+    RETURNS
+        results (pandas.DataFrame) of estimated beta coefficients
+    """
+    pass
+
+
+def load_hospital_data():
+    """
+    Loads the hospital charges data set found at data.gov.
+    INPUT
+        path_to_data (str) indicates the filepath to the hospital charge data (csv)
+    RETURNS
+        clean_df (pandas.DataFrame) containing the cleaned and formatted dataset for regression
+    """
+    pass
+
+
+def prepare_data():
+    """
+    Prepares hospital data for regression (basically turns df into X and y).
+    INPUT
+        df (pandas.DataFrame) the hospital dataset
+    RETURNS
+        data (dict) containing X design matrix and y response variable
+    """
+    pass
+
+
+def run_hospital_regression():
+    """
+    Loads hospital charge data and runs OLS on it.
+    INPUT
+        path_to_data (str) filepath of the csv file
+    RETURNS
+        results (str) the statsmodels regression output
+    """
+    pass
+ 
+
+### END ###
+
+#aaa
